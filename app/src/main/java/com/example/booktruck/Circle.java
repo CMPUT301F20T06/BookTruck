5266d05ead8d729a649eecce9d623f5c0ed3dc47
Yanlin <yanlin2@ualberta.ca>
2020-10-06 11:47:35 -0600

    circle added

---
+++ app/src/main/java/com/example/booktruck/Circle.java
@@ -0,0 +1,23 @@
+package com.example.booktruck;
+
+public class Circle extends Shape {
+    private int x;
+    private int y;
+    private String color = "red";
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
