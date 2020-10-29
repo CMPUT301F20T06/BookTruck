fb1f029f31c91ac2e8d131db7733a7bb4d3042d0
GraceFu <46909395+GraceFu@users.noreply.github.com>
2020-10-28 18:09:32 -0600

    profile ui

--- app/src/main/java/com/example/booktruck/MainActivity.java
+++ app/src/main/java/com/example/booktruck/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -14,6 +16,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private Button profileButton;
 
     public void checkAuth() {
         FirebaseUser currentUser = mAuth.getCurrentUser();
@@ -27,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        profileButton = findViewById(R.id.action_profile);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(this, )
+            }
+        });
 
         mAuth = FirebaseAuth.getInstance();
     }
