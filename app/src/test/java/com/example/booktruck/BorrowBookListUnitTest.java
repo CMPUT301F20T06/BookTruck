da5f3a2bdc99a83054ffeedcd00ab2ece05cb987
“xutong-li” <lixutong99@gmail.com>
2020-11-06 04:52:04 -0700

    borrowBookListUnitTest

---
+++ app/src/test/java/com/example/booktruck/BorrowBookListUnitTest.java
@@ -0,0 +1,40 @@
+package com.example.booktruck;
+
+import android.util.Log;
+import com.google.firebase.auth.FirebaseAuth;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class BorrowBookListUnitTest {
+    FirebaseAuth mAuth;
+
+    private BorrowBookList mockBorrwoBookList() {
+        BorrowBookList borrowBookList = new BorrowBookList();
+        return borrowBookList;
+    };
+
+    @Before
+    public void signIn(){
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Test
+    void testGetCurrentUserName() {
+        BorrowBookList borrowBookList = mockBorrwoBookList();
+//        assertEquals("test@gmail.com", mAuth.getCurrentUser().getEmail());
+
+        borrowBookList.setEmail("test@gamil.com");
+        assertEquals("test", borrowBookList.getCurrentUsername());
+
+        borrowBookList.setEmail("test222@gamil.com");
+        assertEquals("test222", borrowBookList.getCurrentUsername());
+
+    }
+
+
+}
