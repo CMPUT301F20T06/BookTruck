d7e16c11799c1fa5a55cbd2144147a9a3db03e29
Yifan <yifan9799@gmail.com>
2020-11-06 14:12:06 -0700

    add

---
+++ app/src/androidTest/java/com/example/booktruck/ProfilePageTest.groovy
@@ -0,0 +1,16 @@
+package com.example.booktruck
+
+class ProfilePageTest extends groovy.util.GroovyTestCase {
+    void setUp() {
+        super.setUp()
+    }
+
+    void testGetCurrentUsername() {
+    }
+
+    void testOnCreate() {
+    }
+
+    void testOnActivityResult() {
+    }
+}
